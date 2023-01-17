dfbed2127b9ab0be0900d036d0e83f15ecc9f4b7
1031531798 <1031531798@qq.com>
2023-01-17 09:35:57 +0800

    first commit

---
+++ src/main/java/com/lou/springboot/common/Result.java
@@ -0,0 +1,56 @@
+package com.lou.springboot.common;
+
+import java.io.Serializable;
+
+
+public class Result<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int code;
+    private String message;
+    private T data;
+
+    public Result() {
+    }
+
+    public Result(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getResultCode() {
+        return code;
+    }
+
+    public void setResultCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Result failure(String code) {
+        return new Result(500, "服务错误");
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
